2a0e463190984ff7e73ef5e6b435111143a42f3a
Tanimowo Emmanuel <oluwasegunprosperity@gmail.com>
2021-09-01 15:01:54 +0100

    add

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,12 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import Post
 
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
-
 
 class BlogDetailView(DetailView):
     model = Post
@@ -17,8 +17,12 @@ class BlogCreateView(CreateView):
     template_name = 'post_new.html'
     fields = ['title', 'author', 'body']
 
-
 class BlogUpdateView(UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
